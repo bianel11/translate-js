c32b39dfe867fde1f651b2a6135cef0f4217baed
Bianel <bianel9815@gmail.com>
2022-12-14 11:15:55 -0400

    change example

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> </head> <body> <?php
-function sumar($a, $b)
+function imprimir($a)
 {
-    echo $a + $b;
+    echo $a;
 }
-sumar(2, 3);
+$a = "hola";
+imprimir($a);
  ?> </body> </html>
